090c0ebe65c37a1257a6874ef7c506db02fc9491
NAGARJUNA2005 <nagarjuna.vitaliya1@gmail.com>
2025-10-31 15:30:50 +0530

    SEO + favicon updates

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig({build: {
+  minify: 'esbuild',
+  cssMinify: true,
+  chunkSizeWarningLimit: 1000,
+},
+
   plugins: [react()],
 });
